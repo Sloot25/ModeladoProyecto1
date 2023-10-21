6964856f386af19fe11be5114e57761480c4d7b7
Carlos Razo <Konquistad@gmail.com>
2023-10-20 19:12:48 -0600

    modificando los estados iniciar e iniciarSesion

--- src/State/Iniciar.java
+++ src/State/Iniciar.java
@@ -1,38 +1,59 @@
-import java.util.Scanner; 
-public class Iniciar {
+package State;
+import Proxy.User;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Iniciar implements EstadoMark{
   CheemsMark chemsito; 
-  Hashmap<String, User> map;
+  HashMap<String, User> map;
 
   public Iniciar(CheemsMark chemsito){
     this.chemsito = chemsito;
-    this.map = new Hashmap<String, User>();
+    this.map = new HashMap<String, User>();
   }
 
+  public void inicializarEstado(){
+    throw new UnsupportedOperationException();
+  }
   public void ingresar(){
-    throw new UnSupportedOperationException();
+    throw new UnsupportedOperationException();
   }
   public void verCatalogo(){
-    throw new UnSupportedOperationException(); 
+    throw new UnsupportedOperationException(); 
   }
   public void comprar(){
-    throw new UnSupportedOperationException(); 
+    throw new UnsupportedOperationException(); 
   }
   public void cerrarSesion(){
-    throw new UnSupportedOperationException();
+    throw new UnsupportedOperationException();
   }
   public void IniciarSesion(){
     Scanner in = new Scanner(System.in);
-    System.out.println(chemsito.getUsuario().getIdioma().saludar());
-    System.out.println(chemsito.getUsuario().getIdioma().ingresarUsuario());
+    System.out.println("****************************************");
+    System.out.println("*                                      *");
+    System.out.println("*        Bienvenido a CheemsMark       *");
+    System.out.println("*                                      *");
+    System.out.println("****************************************\n");
+    System.out.println("Por favor, ingrese su usuario: ");
     String nombre = in.nextLine();
-    System.out.println(chemsito.getUsuario().getIdioma().ingresarContrasenia());
+    System.out.println("Por favor, ingrese su contraseña: ");
     String contrasenia = in.nextLine();
-    if(map.get(nombre).validarUsuario(contrasenia))
-      chemsito.setEstado(chemsito.getIniciarSesion());
-    else 
+    if(map.get(nombre).validarUsuario(contrasenia)){
+      chemsito.setEstado(chemsito.getEstadoIniciarSesion());
+      chemsito.inicializarEstado();
+    }
+    else{ 
       System.err.println("Tu usuario y/o contrasenia es incorrecta");
+    }
   }
   public void salir(){
-    System.out.println(chemsito.getUsuario().getIdioma().despedirse());
+    System.out.println("Finalizando el programa...");
   }
 }
+  
+
+
+
+
+
+
--- src/State/IniciarSesion.java
+++ src/State/IniciarSesion.java
@@ -1,18 +1,48 @@
-public class IniciarSesion {
-  CheemsMark chemsito; 
+import java.util.Scanner;
+import Composite.CatalogoComponente;
+import Composite.MenuDepartamental;
+
+public class IniciarSesion implements EstadoMark{
+  CheemsMark chemsito;
+  MenuDepartamental catalogo = new MenuDepartamental(chemsito); 
 
   public IniciarSesion(CheemsMark chemsito){
     this.chemsito = chemsito;
   }
   public void inicializarEstado(){
-    System.out.println(chemsito.getUsuario().getIdioma().saludoInicio());
-    System.out.print(chemsito.getUsuario().getIdioma().descuento());
-    System.out.println(" " + chemsito.getUsuario().getDescuento());
+    System.out.println(chemsito.getUsuario().getIdioma().saludar());
+    Scanner in = new Scanner(System.in);
+    while(true){
+    String opcionUsuario = in.nextLine();
+    int opcion; 
+    try {
+        opcion = Integer.parseInt(opcionUsuario);
+    } catch (NumberFormatException e) {
+      System.out.println("Error: Debe ingresar un número entero. Intente nuevamente.");
+        continue;
+    }
+    switch(opcion){
+      case 1:
+        chemsito.verCatalogo();
+        break;
+      case 2:
+        chemsito.comprar();
+        break;
+      case 3:
+        chemsito.cerrarSesion();
+        break;
+      case 4:
+        chemsito.salir();
+        break;
+      default:
+        System.out.println("Error. Debe ingresar una opcion valida.");
+      }
+    }
   }
 
   public void verCatalogo(){
     chemsito.setEstado(chemsito.getEstadoVerCatalogo());
-    chemsito.incializarEstado(); 
+    chemsito.inicializarEstado();
   }
   public void comprar(){
     chemsito.setEstado(chemsito.getEstadoComprar());
@@ -20,11 +50,10 @@ public class IniciarSesion {
   }
   public void cerrarSesion(){
     chemsito.setEstado(chemsito.getEstadoIniciar()); 
-    System.out.println(chemsito.getUsuario().getIdioma().cerrarSesion());
     chemsito.inicializarEstado();
   }
   public void IniciarSesion(){
-    System.out.println(chemsito.getUsuario().getIdioma().sesionIniciada());
+    throw new UnsupportedOperationException();
   }
   public void salir(){
     System.out.println(chemsito.getUsuario().getIdioma().despedirse());
