fc160ca47aa516696a73c58f76fb92d194b868ed
Sloot25 <vidalaguilardiego@ciencias.unam.mx>
2023-10-21 21:14:14 -0600

    Cambiando Idioma

--- src/Strategy/Idioma.java
+++ src/Strategy/Idioma.java
@@ -7,4 +7,17 @@ public interface Idioma{
   public String completarCompra();
   public String despedirse();
   public String saludar();
+  public String repeticionCatalogo();
+  public String cerrarSesion();
+  public String sesionIniciada();
+  public String bienvenidaCompra();
+  public String costo();
+  public String continuarCompra();
+  public String escogeOpcion();
+  public String noEsNumero();
+  public String seguirComprando();
+  public String codigoIncorrecto();
+  public String iniciandoCompra();
+  public String compraFinalizada();
+  public String baneado();
 }
