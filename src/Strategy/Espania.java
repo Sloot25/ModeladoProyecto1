b2c86e0c78153e4e5c7de398549df1ef9c512409
Sloot25 <vidalaguilardiego@ciencias.unam.mx>
2023-10-21 21:11:54 -0600

    Cambios a Espania

--- src/Strategy/Espania.java
+++ src/Strategy/Espania.java
@@ -28,10 +28,75 @@ public class Espania implements Idioma{
   }
 
   public String despedirse(){
-    return "Hasta luego Chavalin, esssspero vuestro regreso para pillar nuestras cosillas";
+    return "Hasta aqui la compra de hoy chavales, no olviden suscribirse y picarle a la campanita, esssspero vuestro regreso para pillar nuestras cosillas";
   }
 
   public String completarCompra(){
     return "Le agradezzzco por su compra, si algun dia quiere pillar otras cosillas le agradeceria que pillara las nuestras";
+  }
+
+  @Override
+  public String repeticionCatalogo() {
+    return "Pero que me contas tio, que ya estas viendo el catalogo";
+  }
+
+  @Override
+  public String cerrarSesion() {
+    return "Cerrando sesión chaval, hasta la proxima";
+  }
+
+  @Override
+  public String sesionIniciada() {  
+    return "Pero que haceis tio, que ya iniciaste sesion";
+  }
+
+  @Override
+  public String bienvenidaCompra() {
+    return "Hey muy buenas a todos guapisimos, aqui CheemsMark777 en una nueva compra ¿que es lo vais a querer?";
+  }
+
+  @Override
+  public String costo() {
+    return "Estos productos estan que flipas tio, tendreis que pagar ";
+  }
+
+  @Override
+  public String continuarCompra() {
+    return "¿Queréis continuar con la compra chaval? (1. Si / 2. No)";
+  }
+
+  @Override
+  public String escogeOpcion() {
+    return "¿Pero que ha pasao? Elige una de las opciones";
+  }
+
+  @Override
+  public String noEsNumero() {
+    return "No fastidies tio, que ese ni es un numero";
+  }
+
+  @Override
+  public String seguirComprando() {
+    return "¿Queréis comprar algo mas? Os recomiendo aprovechar y llevaros algo mas";
+  }
+
+  @Override
+  public String codigoIncorrecto() {
+    return "Pero que haceis tio, ese codigo es incorrecto";
+  }
+
+  @Override
+  public String iniciandoCompra() {
+    return "Vale tio, ahora solo necesito que me deis vuestros datos de pago";
+  }
+
+  @Override
+  public String compraFinalizada() {
+    return "Se ha completado la compra chaval, os agradezco por vuestra compra";
+  }
+
+  @Override
+  public String baneado() {
+    return "NOOO LA CUENTA TIOOO QUE TE EQUIVOCASTE TRES VECES QUE TE EQUIVOCASTE TRES VECES, NO HAGAIS ESE ERROR, QUE TE BANEAMOS CHAVAL";
   }
 }
